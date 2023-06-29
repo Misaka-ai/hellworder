328da42b680c9335f5f92e67fae1f522c49e5f70
liudong.li <liudong.li@foxmail.com>
2023-06-29 11:08:31 +0800

    Init

---
+++ src/test/java/com/itheima/linuxdeploydemo/LinuxDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.linuxdeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxDeployDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
